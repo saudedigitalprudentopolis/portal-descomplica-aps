6fff87dfba6720294bd24eadb267b3a7502eeabe
saudedigitalprudentopolis <infsaudeprudentopolis@gmail.com>
2026-09-18 12:12:00 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.PORTAL_CONFIG = {
-  // Cole aqui a URL gerada ao publicar o Google Apps Script como aplicativo da web.
-  apiUrl: "",
-  fallbackDataUrl: "data/portal-data.json",
-  siteName: "Portal Descomplica",
-  municipality: "Prudentópolis"
-};
